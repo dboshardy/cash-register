101b081f832b1af55569f6c6b0b8cc85dc5fffcb
dboshardy <dbosh09@gmail.com>
2017-10-22 18:53:09 -0500

    initial scaffolding

---
+++ src/com/dboshardy/cashregister/CashRegisterRunner.kt
@@ -0,0 +1,4 @@
+package com.dboshardy.cashregister
+
+class CashRegisterRunner {
+}
---
+++ src/com/dboshardy/cashregister/model/CashRegister.kt
@@ -0,0 +1,4 @@
+package com.dboshardy.cashregister.model
+
+data class CashRegister(twenties: Int, tens: Int, fives: Int, twos: Int, ones: Int)
+}
