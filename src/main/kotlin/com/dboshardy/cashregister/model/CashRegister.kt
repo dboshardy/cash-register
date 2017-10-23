6fc856bc336398c917c67430344d9157dadfb8f3
dboshardy <dbosh09@gmail.com>
2017-10-22 20:48:07 -0500

    add helper methods to get bill numbers, corrected input list to put bills test and the message text

--- src/main/kotlin/com/dboshardy/cashregister/model/CashRegister.kt
+++ src/main/kotlin/com/dboshardy/cashregister/model/CashRegister.kt
@@ -3,15 +3,15 @@ package com.dboshardy.cashregister.model
 import kotlin.coroutines.experimental.EmptyCoroutineContext.plus
 
 private val PROPER_INPUT_FORMAT = "TWENTIES TENS FIVES TWOS ONES"
-private val NUM_BILL_TYPES = 5
 private val TWENTIES_INDEX = 0
 private val TENS_INDEX = 1
 private val FIVES_INDEX = 2
 private val TWOS_INDEX = 3
 private val ONES_INDEX = 4
 
-data class CashRegister(private var bills: List<Int>) {
-    constructor(twenties: Int = 0, tens: Int = 0, fives: Int = 0, twos: Int = 0, ones: Int = 0) : this(listOf(twenties, tens, fives, twos, ones))
+data class CashRegister(private var bills: List<Int> = listOf(0,0,0,0,0)) {
+    constructor(twenties: Int, tens: Int, fives: Int, twos: Int, ones: Int) : this(listOf(twenties, tens, fives, twos, ones))
+    private val NUM_BILL_TYPES = bills.size
 
 
     /*
@@ -22,37 +22,36 @@ data class CashRegister(private var bills: List<Int>) {
             return "Could not add bills, please specify bills in the proper format: 'put ${PROPER_INPUT_FORMAT}'"
         }
 
-        this.bills.zip(bills).map{ it.first + it.second }
+        this.bills = this.bills.zip(bills).map{ it.first + it.second }
 
         return show()
     }
 
     /*
      * Takes an array of bills assumed to be in proper order and takes them out of the register
-     * TODO: Make sure you cannot take more than the present number of each bill out of the register
      */
     fun take(bills: List<Int>): String {
         if (bills.size != NUM_BILL_TYPES) {
             return "Could not add bills, please specify bills in the proper format: 'put ${PROPER_INPUT_FORMAT}'"
         }
 
-        this.bills.zip(bills).map{ it.first - it.second }
-        correctNegativeCounts()
+        this.bills = this.bills.zip(bills).map{ takeBills(it.first, it.second) }
 
         return show()
     }
 
-    private fun correctNegativeCounts() {
-        bills = bills.map { makeNegativeZero(amount = it) }
+    /*
+     * Removes bills from register, only removes as many are present
+     */
+    private fun takeBills(billsInRegister: Int, newBills: Int): Int {
+        return if(newBills > billsInRegister) {
+            0
+        } else {
+            billsInRegister - newBills
+        }
+
     }
 
-    /*
-     * takes any negative bill amount and converts to 0
-     * TODO: Change how this works such that you cannot take more bills than are in the register out
-     */
-    private fun makeNegativeZero(amount: Int): Int {
-        return if (amount < 0) 0 else amount
-    }
 
     /*
      * returns the total amount of money in the register along with the number of each kind of bill in the proper format
@@ -80,4 +79,24 @@ data class CashRegister(private var bills: List<Int>) {
 
     }
 
+
+    fun twenties(): Int {
+        return bills[TWENTIES_INDEX]
+    }
+
+    fun tens(): Int {
+        return bills[TENS_INDEX]
+    }
+
+    fun fives(): Int {
+        return bills[FIVES_INDEX]
+    }
+
+    fun twos(): Int {
+        return bills[TWOS_INDEX]
+    }
+
+    fun ones(): Int {
+        return bills[ONES_INDEX]
+    }
 }
--- src/test/com/dboshardy/cashregister/CashRegisterTest.kt
+++ src/test/com/dboshardy/cashregister/CashRegisterTest.kt
@@ -5,37 +5,34 @@ import org.junit.Before
 import kotlin.test.assertEquals
 
 class CashRegisterTest {
-    private val register: CashRegister = CashRegister() // empty register
+    private var register: CashRegister = CashRegister() // empty register
 
     @Before
     fun setUp() {
-        register.twenties = 5
-        register.tens = 5
-        register.fives = 5
-        register.twos = 5
-        register.ones = 5
+        val bills = listOf(5, 5, 5, 5, 5)
+        register = CashRegister(bills)
     }
 
     @org.junit.Test
     fun testPutBills() {
-        val bills = arrayOf(1, 2, 3, 5, 6)
+        val bills = listOf(1, 2, 3, 4, 5)
         register.put(bills)
-        assertEquals(expected = 6, actual = register.twenties, message = "After adding 1 bill to the twenties, register should have 6 twenties")
-        assertEquals(expected = 7, actual = register.tens, message = "After adding 1 bill to the tens, register should have 7 twenties")
-        assertEquals(expected = 8, actual = register.fives, message = "After adding 1 bill to the fives, register should have 8 fives")
-        assertEquals(expected = 9, actual = register.twos, message = "After adding 1 bill to the twos, register should have 9 twos")
-        assertEquals(expected = 10, actual = register.ones, message = "After adding 1 bill to the ones, register should have 10 ones")
+        assertEquals(expected = 6, actual = register.twenties(), message = "After adding 1 bill to the twenties, register should have 6 twenties")
+        assertEquals(expected = 7, actual = register.tens(), message = "After adding 2 bill to the tens, register should have 7 twenties")
+        assertEquals(expected = 8, actual = register.fives(), message = "After adding 3 bill to the fives, register should have 8 fives")
+        assertEquals(expected = 9, actual = register.twos(), message = "After adding 4 bill to the twos, register should have 9 twos")
+        assertEquals(expected = 10, actual = register.ones(), message = "After adding 5 bill to the ones, register should have 10 ones")
     }
 
     @org.junit.Test
     fun testTakeBills() {
-        val bills = arrayOf(1, 2, 3, 4, 5)
+        val bills = listOf(1, 2, 3, 4, 5)
         register.take(bills)
-        assertEquals(expected = 4, actual = register.twenties, message = "After taking away 1 bill to the twenties, register should have 4 twenties")
-        assertEquals(expected = 3, actual = register.tens, message = "After taking away 2 bill to the tens, register should have 3 twenties")
-        assertEquals(expected = 2, actual = register.fives, message = "After taking away 3 bill to the fives, register should have 2 fives")
-        assertEquals(expected = 1, actual = register.twos, message = "After taking away 4 bill to the twos, register should have 1 twos")
-        assertEquals(expected = 0, actual = register.ones, message = "After taking away 5 bill to the ones, register should have 0 ones")
+        assertEquals(expected = 4, actual = register.twenties(), message = "After taking away 1 bill to the twenties, register should have 4 twenties")
+        assertEquals(expected = 3, actual = register.tens(), message = "After taking away 2 bill to the tens, register should have 3 twenties")
+        assertEquals(expected = 2, actual = register.fives(), message = "After taking away 3 bill to the fives, register should have 2 fives")
+        assertEquals(expected = 1, actual = register.twos(), message = "After taking away 4 bill to the twos, register should have 1 twos")
+        assertEquals(expected = 0, actual = register.ones(), message = "After taking away 5 bill to the ones, register should have 0 ones")
     }
 
     @org.junit.Test
@@ -47,5 +44,4 @@ class CashRegisterTest {
     fun testMakeChange() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
-
 }
