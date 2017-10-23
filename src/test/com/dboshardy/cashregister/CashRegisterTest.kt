767026e4807f68cfd631d037a4e82adbf6b6d51e
dboshardy <dbosh09@gmail.com>
2017-10-22 20:05:46 -0500

    writing simpler tests

--- src/test/com/dboshardy/cashregister/CashRegisterTest.kt
+++ src/test/com/dboshardy/cashregister/CashRegisterTest.kt
@@ -2,29 +2,47 @@ package com.dboshardy.cashregister
 
 import com.dboshardy.cashregister.model.CashRegister
 import org.junit.Before
+import kotlin.test.assertEquals
 
 class CashRegisterTest {
-    val register: CashRegister = CashRegister() // empty register
+    private val register: CashRegister = CashRegister() // empty register
 
     @Before
     fun setUp() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        register.twenties = 5
+        register.tens = 5
+        register.fives = 5
+        register.twos = 5
+        register.ones = 5
     }
 
     @org.junit.Test
     fun testPutBills() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val bills = arrayOf(1, 2, 3, 5, 6)
+        register.put(bills)
+        assertEquals(expected = 6, actual = register.twenties, message = "After adding 1 bill to the twenties, register should have 6 twenties")
+        assertEquals(expected = 7, actual = register.tens, message = "After adding 1 bill to the tens, register should have 7 twenties")
+        assertEquals(expected = 8, actual = register.fives, message = "After adding 1 bill to the fives, register should have 8 fives")
+        assertEquals(expected = 9, actual = register.twos, message = "After adding 1 bill to the twos, register should have 9 twos")
+        assertEquals(expected = 10, actual = register.ones, message = "After adding 1 bill to the ones, register should have 10 ones")
     }
 
     @org.junit.Test
     fun testTakeBills() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val bills = arrayOf(1, 2, 3, 4, 5)
+        register.take(bills)
+        assertEquals(expected = 4, actual = register.twenties, message = "After taking away 1 bill to the twenties, register should have 4 twenties")
+        assertEquals(expected = 3, actual = register.tens, message = "After taking away 2 bill to the tens, register should have 3 twenties")
+        assertEquals(expected = 2, actual = register.fives, message = "After taking away 3 bill to the fives, register should have 2 fives")
+        assertEquals(expected = 1, actual = register.twos, message = "After taking away 4 bill to the twos, register should have 1 twos")
+        assertEquals(expected = 0, actual = register.ones, message = "After taking away 5 bill to the ones, register should have 0 ones")
     }
 
     @org.junit.Test
     fun testShowBillsInRegister() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        assertEquals(expected = "$${5 * 20 + 10 * 5 + 5 * 5 + 2 * 5 + 5} 5 5 5 5 5", actual = register.show(), message = "Register should output total value and all bills contained within register when show() is called.")
     }
+
     @org.junit.Test
     fun testMakeChange() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
