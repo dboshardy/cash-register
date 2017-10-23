f6212c20a002d82c156db99ed7124fa676f0895e
dboshardy <dbosh09@gmail.com>
2017-10-23 17:45:02 -0500

    add more cases for making change, could use more likely

--- src/test/com/dboshardy/cashregister/CashRegisterTest.kt
+++ src/test/com/dboshardy/cashregister/CashRegisterTest.kt
@@ -53,11 +53,48 @@ class CashRegisterTest {
     @org.junit.Test
     fun testMakeChange() {
         val amount = 43
-        register.makeChange(amount)
+        val change = register.makeChange(amount).split(" ").map { it.toInt()}
         assertEquals(expected = 3, actual = register.twenties(), message = "After making change, register now has 0 bills")
         assertEquals(expected = 5, actual = register.tens(), message = "After making change, register now has 0 bills")
         assertEquals(expected = 5, actual = register.fives(), message = "After making change, register now has 0 bills")
         assertEquals(expected = 4, actual = register.twos(), message = "After making change, register now has 0 bills")
         assertEquals(expected = 4, actual = register.ones(), message = "After making change, register now has 0 bills")
+
+        assertEquals(expected = 2, actual = change[0], message = "After making change for $43, change should show proper amount of twenties (2)")
+        assertEquals(expected = 0, actual = change[1], message = "After making change for $43, change should show proper amount of tens (0)")
+        assertEquals(expected = 0, actual = change[2], message = "After making change for $43, change should show proper amount of fives (0)")
+        assertEquals(expected = 1, actual = change[3], message = "After making change for $43, change should show proper amount of twos (1)")
+        assertEquals(expected = 1, actual = change[4], message = "After making change for $43, change should show proper amount of ones (1)")
+    }
+
+    @org.junit.Test
+    fun testMakeChangeWithLargeAmounts() {
+        register.put(listOf(10000000, 10000000, 10000000, 10000000, 10000000))
+        val amount = 123456789
+        val change = register.makeChange(amount).split(" ").map { it.toInt()}
+
+        // 123456789 / 20 == 6172839.45
+        // 123456789 % 20 == 9
+        // 6172839 twenties
+        // 1 five
+        // 2 twos
+
+        assertEquals(expected = 3827166, actual = register.twenties(), message = "After making change, register now has 0 bills")
+        assertEquals(expected = 10000005, actual = register.tens(), message = "After making change, register now has 0 bills")
+        assertEquals(expected = 10000004, actual = register.fives(), message = "After making change, register now has 0 bills")
+        assertEquals(expected = 10000003, actual = register.twos(), message = "After making change, register now has 0 bills")
+        assertEquals(expected = 10000005, actual = register.ones(), message = "After making change, register now has 0 bills")
+
+        assertEquals(expected = 6172839, actual = change[0], message = "After making change for $, change should show proper amount of twenties (2)")
+        assertEquals(expected = 0, actual = change[1], message = "After making change for $43, change should show proper amount of tens (0)")
+        assertEquals(expected = 1, actual = change[2], message = "After making change for $43, change should show proper amount of fives (0)")
+        assertEquals(expected = 2, actual = change[3], message = "After making change for $43, change should show proper amount of twos (1)")
+        assertEquals(expected = 0, actual = change[4], message = "After making change for $43, change should show proper amount of ones (1)")
+    }
+    @org.junit.Test
+    fun testCannotMakeChange() {
+        register.take(listOf(5, 2, 3, 5, 5))
+        assertEquals(expected = "I apologize, but I cannot make change for $512 with the bills I have.\n$40 0 3 2 0 0", actual = register.makeChange(512), message = "When total in register is less than requested amount to make change for, error should be returned")
+        assertEquals(expected = "I apologize, but I cannot make change for $33 with the bills I have.\n$40 0 3 2 0 0", actual = register.makeChange(33), message = "When register cannot make change with bills present, error message should be returned.")
     }
 }
